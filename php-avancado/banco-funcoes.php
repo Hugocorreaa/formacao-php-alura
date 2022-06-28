2cef031025f8f2af6d86fef01255c89b79f5e501
Hugo Corrêa <hugodemorais@live.com>
2022-06-28 09:03:21 -0300

    dia 28

---
+++ php-avancado/banco-funcoes.php
@@ -0,0 +1,33 @@
+<?php
+
+
+function sacar($conta, $valorASacar)
+{
+    if ($valorASacar > $conta['saldo']){
+        exibeMensagem( $conta['titular'] . ", você não pode sacar este valor!");
+    }else{
+        $conta['saldo'] -= $valorASacar;
+    }
+
+    return $conta;
+};
+
+function depositar($conta, $valorADepositar)
+{
+    if ($valorADepositar > 0){
+        $conta['saldo'] += $valorADepositar;
+    }else{
+        exibeMensagem($conta['titular'] . ", depósitos precisam ser positivos!");
+    }
+    
+    return $conta;
+};
+
+function exibeMensagem($mensagem)
+{
+    echo $mensagem . PHP_EOL;
+};
+
+function titularComLetrasMaiusculas(&$conta){
+   $conta['titular'] = strtoupper($conta['titular']);
+}
--- php-avancado/banco.php
+++ php-avancado/banco.php
@@ -1,58 +1,34 @@
 <?php
 
-function sacar(array $conta, float $valorASacar)
-{
-    if ($valorASacar > $conta['saldo']){
-        exibeMensagem( $conta['titular'] . ", você não pode sacar este valor!");
-    }else{
-        $conta['saldo'] -= $valorASacar;
-    }
-
-    return $conta;
-};
-
-function depositar(array $conta, float $valorADepositar)
-{
-    if ($valorADepositar > 0){
-        $conta['saldo'] += $valorADepositar;
-    }else{
-        exibeMensagem($conta['titular'] . ", depósitos precisam ser positivos!");
-    }
-    
-    return $conta;
-};
-
-function exibeMensagem(string $mensagem)
-{
-    echo $mensagem . PHP_EOL;
-};
+require 'banco-funcoes.php';
 
 $contasCorrentes = [
-    '011.206.458-21' => [
+    '011' => [
         'titular' => 'Maria',
         'saldo' => 10000
     ], 
-    '012.206.458-21' => [
+    '012' => [
         'titular' => 'Alberto',
         'saldo' => 300
     ], 
-    '013.206.458-21' => [
+    '013' => [
         'titular' => 'Vinicius',
         'saldo' => 100
     ]
 ];
 
 
-$contasCorrentes['011.206.458-21'] = sacar($contasCorrentes['011.206.458-21'], 500);
+$contasCorrentes['011'] = sacar($contasCorrentes['011'], 500);
 
-$contasCorrentes['012.206.458-21'] = sacar($contasCorrentes['012.206.458-21'], 500);
+$contasCorrentes['012'] = sacar($contasCorrentes['012'], 500);
 
-$contasCorrentes['013.206.458-21'] = depositar($contasCorrentes['013.206.458-21'], 200);
+$contasCorrentes['013'] = depositar($contasCorrentes['013'], 200);
 
-
-
+titularComLetrasMaiusculas($contasCorrentes['011']);
 
 
 foreach($contasCorrentes as $cpf => $conta){
-    exibeMensagem(str_repeat("-=", 20) . PHP_EOL . "CPF: $cpf" . PHP_EOL . "Nome: " . $conta['titular'] . PHP_EOL . "Saldo: $" . $conta['saldo']);    
+    exibeMensagem(
+        str_repeat("-=", 20) . PHP_EOL . "CPF: $cpf" . PHP_EOL . "Nome: " . $conta['titular'] . PHP_EOL . "Saldo: $" . $conta['saldo']
+    );    
 }
